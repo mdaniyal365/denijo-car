721def01f5a45979fc087d7dddad0002232c1e5e
daniyal <mohddaniyal365@gmail.com>
2023-12-19 22:40:51 +0530

    del

---
+++ src/pages/ListTrip.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListTrip() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListTrip
---
+++ src/pages/RequestRider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RequestRider() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RequestRider
